44982f467a7a8e4798e60a8c97d5d8fb0239f5d5
DA SILVA <davidfctt@gmail.com>
2022-01-04 17:07:55 +0100

    Ajout viewController pour RecipeDetail

---
+++ Reciplease/Reciplease/Controller/RecipeDetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RecipeDetailViewController.swift
+//  Reciplease
+//
+//  Created by David Da Silva on 31/12/2021.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var listIngredients: UITextView!
+    @IBOutlet weak var buttonFavorite: UIBarButtonItem!
+    
+    // MARK: - Actions
+    @IBAction func pressedFavorite(_ sender: UIBarButtonItem) {
+        if buttonFavorite.tintColor == UIColor.white {
+            buttonFavorite.tintColor = UIColor(red: 255/255.0, green: 230/255.0, blue: 0/255.0, alpha: 1)
+            
+        }
+        else if  buttonFavorite.tintColor == UIColor(red: 255/255.0, green: 230/255.0, blue: 0/255.0, alpha: 1) {
+            buttonFavorite.tintColor = UIColor.white
+        }
+    }
+    
+    @IBAction func webLink(_ sender: Any) {
+        if let url = NSURL(string: "https://www.apple.com/fr/") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    // MARK: - Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ingredients()
+        // Do any additional setup after loading the view.
+    }
+    
+    func ingredients() {
+        listIngredients.text = "- Salad \n- Tomatp \n- Egg"
+    }
+    
+}
