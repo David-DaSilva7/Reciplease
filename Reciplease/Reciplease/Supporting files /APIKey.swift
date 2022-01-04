37ee164322cfca7032eb55062798a0b41872d0b7
DA SILVA <davidfctt@gmail.com>
2022-01-04 17:09:14 +0100

    Integration des APIKey WebService

---
+++ Reciplease/Reciplease/Supporting files /APIKey.swift
@@ -0,0 +1,12 @@
+//
+//  APIKey.swift
+//  Reciplease
+//
+//  Created by David Da Silva on 03/01/2022.
+//
+
+
+struct APIKey {
+    static let EdamamRecipeSearchAppID = "29d863ec"
+    static let EdamamRecipeSearchAppKey = "5e5a55dc99f129212f06f212577871eb"
+}
